1bccde6bfddd4be4a4c6fc3710f0ee838713fbb2
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2023-02-28 00:38:49 +0200

    fix

--- admin_bot.py
+++ admin_bot.py
@@ -3,4 +3,4 @@ from dotenv import dotenv_values
 
 """Settings"""
 config = dotenv_values(".env")
-GPT_TOKEN_PERSONAL = config['ADMIN_TOKEN']
+ADMIN_TOKEN = config['ADMIN_TOKEN']
--- client_bot.py
+++ client_bot.py
@@ -3,4 +3,4 @@ from dotenv import dotenv_values
 
 """Settings"""
 config = dotenv_values(".env")
-GPT_TOKEN_PERSONAL = config['CLIENT_TOKEN']
+CLIENT_TOKEN = config['CLIENT_TOKEN']
